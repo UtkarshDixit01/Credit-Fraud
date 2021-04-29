80f4c018b321deea3acb1750083198470041ef38
Shivam Saraswat <shivamsaraswat04@gmail.com>
2021-04-29 12:53:06 +0530

    Update views.py

--- yourstore/views.py
+++ yourstore/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render, HttpResponse
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
-    # return HttpResponse("This Sarswat ka lowra page.")
+    
 def contact(request):
     return render(request, 'contact.html')
 
